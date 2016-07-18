0f74a48713c7895120e84032b527c6452abe529f
Jon Shier <jon@jonshier.com>
2016-07-18 16:28:45 -0400

    Commit sample project.

---
+++ ScanHangTestTests/ScanHangTestTests.swift
@@ -0,0 +1,18 @@
+//
+//  ScanHangTestTests.swift
+//  ScanHangTestTests
+//
+//  Created by Jon Shier on 7/18/16.
+//  Copyright © 2016 Jon Shier. All rights reserved.
+//
+
+import XCTest
+@testable import ScanHangTest
+
+class ScanHangTestTests: XCTestCase {
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+    
+}
